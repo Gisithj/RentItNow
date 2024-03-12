3d90e127492bfe4727e022c3a88ec0233aefa604
gisithj <gisithj@gmail.com>
2024-03-12 17:08:37 +0530

    Added getUser api usinh http cookie

---
+++ Backend/DTOs/User/GetUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RentItNow.DTOs.User
+{
+    public class GetUserDto
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
